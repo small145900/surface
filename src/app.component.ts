67a8117bafb49472e4d85be7d0d672384005a0dc
small145900@163.com <small145900@163.com>
2016-10-08 15:46:28 +0800

    add nav component

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,3 +5,10 @@ import { Component,ViewChild } from '@angular/core';
   template: `<h1>this is surface</h1>`
 })
 export class AppComponent { }
+
+/**
+
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDy0kclNlVyT+kNZo0h/BsNGuskKWTKdzahHfb5dzmZbZH5s5vJQzh2dicZKhK/NVHykXK1brMWmzX7G9ftVufLuV8uLJYjqSNnKMpm/VLY8Bx12rp6dnEEVbelFTHt37pDzftX5mj7a0S8J2n4/U0EeqLkL5rKPssPdPNblItwQxWAQ05NgTMEqT+QUzVMYM3rm7pVNPjjI27pfF9U9SP6Siq73Pv/ZtelkTGWI9kpn8hXciSgLdVT6BWhIXVKEshthsAmR2B8IR5TElK1I7vWlDmckDoo8XIMkDeK6s6m2Ovp9GxZZltaHRuDhcS/3JMjOIjlT/1sriFzr5Z96gbB small145900@163.com
+
+*/
