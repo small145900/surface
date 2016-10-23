d254c000d58c9022fba345fc5dd628faaa7de664
small145900@163.com <small145900@163.com>
2016-10-23 15:29:59 +0800

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,7 @@ rename = require('gulp-rename');
 ts = require('gulp-typescript');
 babel = require('gulp-babel');
 // tsProject = ts.createProject("tsconfig.json");
-
 // tslint = require('gulp-tslint');
-
 // htmlmin = require('gulp-htmlmin');
 // browserify = require('browserify');
 // imagemin = require('gulp-imagemin');
@@ -32,27 +30,6 @@ gulp.task("lib-css",function(){
 		.pipe(gulp.dest("dist/css"));
 });
 
-// gulp.task("minify-lib-js",function(){
-// 	return gulp.src([
-// 			"node_modules/jquery/dist/jquery.min.js",
-// 			"node_modules/bootstrap/dist/js/bootstrap.min.js",
-// 			"node_modules/core-js/client/shim.min.js",
-// 			"node_modules/zone.js/dist/zone.js",
-// 			"node_modules/reflect-metadata/Reflect.js",
-// 			"node_modules/systemjs/dist/system.src.js",
-// 			"systemjs.config.js"
-// 		])
-//    .pipe(concat("lib.min.js"))
-// 		.pipe(uglify())
-// 		.pipe(gulp.dest("dist/js"));
-// });
-
-// gulp.task("less",function(){
-// 	return gulp.src(["less/*.less"])
-// 		.pipe(less())
-// 		.pipe(gulp.dest("css"));
-// });
-
 gulp.task("img",function(){
 	return gulp.src(["img/*"])
 		.pipe(gulp.dest("dist/img"));
@@ -73,76 +50,53 @@ gulp.task("ts",function(){
 		.pipe(gulp.dest("dist/src"));
 });
 
+gulp.task("systemjs",function(){
+	return gulp.src('systemjs.config.js')
+		.pipe(gulp.dest("dist"));
+});
+
 gulp.task("index",function(){
 	return gulp.src(["index.html"])
 		.pipe(gulp.dest("dist"));
 });
 
-
-// gulp.task("watch",  () => {
-//    gulp.watch("less/.less",['less']);
+// gulp.task("lib", () => {
+  // return gulp.src([
+  // 				'jquery/dist/jquery.min.js',
+  // 				'bootstrap/dist/js/bootstrap.min.js',
+  //         'core-js/client/shim.min.js',
+  //         'systemjs/dist/system-polyfills.js',
+  //         'systemjs/dist/system.src.js',
+  //         'reflect-metadata/Reflect.js',
+  //         'rxjs/**/*.js',
+  //         'zone.js/dist/**'
+  //         // '@angular/**/bundles/**'
+  //     ],{cwd: "node_modules/**"})
+//   		.pipe(concat("lib.min.js"))
+//       .pipe(gulp.dest("dist/js"));
 // });
 
-// gulp.task('default', []);
-// gulp.task('default', [
-// 	'img',
-// 	'json',
-// 	'index',
-// 	'templates',
-// 	"src",
-// 	"minify-lib-js",
-// 	'minify-lib-css',
-// 	"minify-css",
-// 	"minify-ts"
-// ])
-// gulp.task("compile", () => {
-// 	return gulp.src(["src/*.ts"])
-// 			.pipe(gulp.dest('dist/js'))
-// })
-// gulp.task('tslint', () => {
-//     return gulp.src("src/*.ts")
-//         .pipe(tslint({
-//             formatter: 'prose'
-//         }))
-//         .pipe(tslint.report());
-// });
-
-// gulp.task("compile", () => {
-//     let tsResult = gulp.src("src/*.ts")
-//         .pipe(tsProject());
-//     return tsResult.js
-//         .pipe(gulp.dest("dist/js"));
-// });
-
-// gulp.task("resources", () => {
-//     return gulp.src(["src/*", "!**/*.ts"])
-//         .pipe(gulp.dest("dist/js"));
-// });
-gulp.task("lib", () => {
-  return gulp.src([
-          'node_modules/core-js/client/shim.min.js',
-          'node_modules/systemjs/dist/system-polyfills.js',
-          'node_modules/systemjs/dist/system.src.js',
-          'node_modules/reflect-metadata/Reflect.js',
-          'node_modules/rxjs/**/*.js',
-          'node_modules/zone.js/dist/**',
-          'node_modules/@angular/**/bundles/**',
-          './systemjs.config.js',
-      ])
-  		.pipe(concat("lib.min.js"))
-      .pipe(gulp.dest("dist/js"));
+gulp.task("lib",function(){
+	return gulp.src([
+			"node_modules/jquery/dist/jquery.min.js",
+			"node_modules/bootstrap/dist/js/bootstrap.min.js",
+			"node_modules/core-js/client/shim.min.js",
+			"node_modules/zone.js/dist/zone.js",
+			"node_modules/reflect-metadata/Reflect.js",
+			"node_modules/systemjs/dist/system.src.js",
+			"systemjs.config.js"
+		])
+    .pipe(concat("lib.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/js"));
 });
 
-
-// gulp.task("minify-ts",function(){
-// 	return gulp.src(["src/*.ts"])
-//     .pipe(ts({
-//     	noImplicitAny: true,
-//     	out: 'app.min.js'
-//     }))
-// 		.pipe(uglify())
-// 		.pipe(gulp.dest("dist/js"));
-// });
+gulp.task("angular",function(){
+	return gulp.src([
+          '@angular/**/bundles/**'
+      ],{cwd: "node_modules/**"})
+      .pipe(gulp.dest("dist/js"));
+});
 
 // gulp.task("minify-js",function(){
 // 	return gulp.src(["ts/*.js"])
@@ -157,8 +111,10 @@ gulp.task('default', [
 	'json',
 	'templates',
 	'ts',
+	'systemjs',
 	'index',
 	'lib-css',
 	'minify-css',
-	'lib'
+	'lib',
+	'angular'
 ])
