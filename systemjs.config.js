c5fd832947f98f44c827003490a5ebaa0f83aeff
small145900@163.com <small145900@163.com>
2016-10-24 21:56:44 +0800

    modify base font

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,8 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'lib/'
+      // 'npm:': 'lib/'
+      'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
